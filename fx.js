373d1947712c257f7ea751adcd1390fd62e5dc49
Felipe Drechsler <74937282+FelipeDrechsler2020@users.noreply.github.com>
2020-12-22 21:44:03 -0300

    Add files via upload

--- fx.js
+++ fx.js
@@ -1,3 +1,4 @@
+var vala, valb;
 function funcaoprim(){
     var nx1 = document.getElementById('txtx1');
     var nx2 = document.getElementById('txtx2');
@@ -23,13 +24,16 @@ function funcaoprim(){
     var ix2 = Number(nx2.value);
     var iy1 = Number(ny1.value);
     var iy2 = Number(ny2.value);
+    //adiconar o valor dos pontos
+    document.getElementById('txa').innerHTML = ix1 + ", " + iy1;
+    document.getElementById('txb').innerHTML = ix2 + ", " + iy2;
     //calcular o valor de a e b:
 
-    var vala = ((iy2-iy1)/(ix2-ix1));
+    vala = ((iy2-iy1)/(ix2-ix1));
     //passar para o label
     document.getElementById('lba').innerHTML = vala;
 
-    var valb = ((iy1*ix2)-(iy2*ix1))/(ix2-ix1);
+    valb = ((iy1*ix2)-(iy2*ix1))/(ix2-ix1);
     //passar para o label
     document.getElementById('lbb').innerHTML = valb;
     //escrever a função
@@ -41,22 +45,13 @@ function funcaoprim(){
     };
 }
 function calvaly(){
-    var nx1 = document.getElementById('txtx1');
-    var nx2 = document.getElementById('txtx2');
-    var ny1 = document.getElementById('txty1');
-    var ny2 = document.getElementById('txty2');
     var nx3 = document.getElementById('verxtxt');
     if (nx3.value.length == 0){
         window.alert('Preencha o campo do valor X com um valor numérico e pressione Calcular Y')
     };
-    
-    var ix1 = Number(nx1.value);
-    var ix2 = Number(nx2.value);
-    var iy1 = Number(ny1.value);
-    var iy2 = Number(ny2.value);
-    var pty = Number(nx3.value);
-    var vala = ((iy2-iy1)/(ix2-ix1));
-    var valb = ((iy1*ix2)-(iy2*ix1))/(ix2-ix1);
+     var pty = Number(nx3.value);
+    //var vala = ((iy2-iy1)/(ix2-ix1));
+    //var valb = ((iy1*ix2)-(iy2*ix1))/(ix2-ix1);
     //f(x) = ax + b
     //
     var ponty = (vala*pty)+valb;
@@ -64,22 +59,13 @@ function calvaly(){
     
 }
 function calvalx(){
-    var nx1 = document.getElementById('txtx1');
-    var nx2 = document.getElementById('txtx2');
-    var ny1 = document.getElementById('txty1');
-    var ny2 = document.getElementById('txty2');
-    var ny3 = document.getElementById('verytxt');
+     var ny3 = document.getElementById('verytxt');
     if (ny3.value.length == 0){
         window.alert('Preencha o campo do valor Y com um valor numérico e pressione Calcular X')
     };
-    
-    var ix1 = Number(nx1.value);
-    var ix2 = Number(nx2.value);
-    var iy1 = Number(ny1.value);
-    var iy2 = Number(ny2.value);
     var ptx = Number(ny3.value);
-    var vala = ((iy2-iy1)/(ix2-ix1));
-    var valb = ((iy1*ix2)-(iy2*ix1))/(ix2-ix1);
+    //var vala = ((iy2-iy1)/(ix2-ix1));
+    //var valb = ((iy1*ix2)-(iy2*ix1))/(ix2-ix1);
     //f(x) = ax + b
     //y = ax + b; y-b = ax; y-b/a = x
     var pontx = (ptx-valb)/vala;
